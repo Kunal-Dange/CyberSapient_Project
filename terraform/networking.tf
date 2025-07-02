9e05e885a1ffc9f0fc3a9bc22efb21707186a562
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-02 20:02:52 +0530

    creating vpc

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source = "./modules/vpc"
     cidr_block = "100.10.0.0/24"
-    vpc_name = "CyberSapient-VPC"
+    vpc_name = "${var.environment}-${var.app_env}-vpc"
     app_env = "dev1"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,10 @@ variable "target_infra" {
     default     = "Nonprod"
 }
 
+variable "environment" {
+  description = "Environment name (prod/nonprod)"
+  type        = string
+}
+variable "app_env" {
+    description = "Application environment" 
+}
