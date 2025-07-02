dd9ef8fb32dc4dec02a1921767e778e2aba6fac9
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-02 15:16:33 +0530

    creating s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "kd-kubernetes"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+  }
 }
 
 provider "aws" {
