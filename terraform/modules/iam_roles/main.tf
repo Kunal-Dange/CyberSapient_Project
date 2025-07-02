e26201f3526823fd2d746c6768e55d8cf6a1da1c
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-02 22:05:01 +0530

    terraform

--- terraform/modules/iam_roles/main.tf
+++ terraform/modules/iam_roles/main.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "roles" {
   for_each = var.iam_roles
   name     = each.value.name
-  assume_role_policy = jsonencode(each.value.assume_role_policy)
+  assume_role_policy = each.value.assume_role_policy
   description        = each.value.description
   max_session_duration = each.value.max_session_duration
   tags               = each.value.tags
