7a09b2dd0beedc6a6e8aa46e24bc48e734615a44
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-03 22:52:31 +0530

    terraform

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -5,11 +5,14 @@ module "vpc" {
     app_env = "dev1"
 }
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "public" {
   count                   = length(data.aws_availability_zones.available.names)
-  vpc_id                  = module.vpc.id
+  vpc_id                  = module.vpc.vpc_id
   cidr_block              = cidrsubnet(module.vpc.cidr_block, 4, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
@@ -21,7 +24,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count             = length(data.aws_availability_zones.available.names)
-  vpc_id            = module.vpc.id
+  vpc_id            = module.vpc.vpc_id
   cidr_block        = cidrsubnet(module.vpc.cidr_block, 4, count.index + 10)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
