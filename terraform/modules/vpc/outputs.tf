3ebcdd9141fc38a2db8ab8cdd25932cd37876c4f
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-03 22:31:32 +0530

    terraform

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -2,3 +2,12 @@ output "id" {
   value = aws_vpc.main.id
   description = "The ID of the VPC"
 }
+output "id" {
+  value       = aws_vpc.main.id
+  description = "The ID of the VPC"
+}
+
+output "cidr_block" {
+  value       = aws_vpc.main.cidr_block
+  description = "The CIDR block of the VPC"
+}
