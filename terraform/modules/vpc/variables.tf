413bc89441a8d227741069848163ef5a70447964
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-03 22:36:34 +0530

    terraform

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,13 +1,16 @@
 variable "app_env" {
     description = "Application environment" 
+    type = string
 }
 
 variable "cidr_block" {
     description = "VCP CIDR Range"  
+    type = string
 }
 
 variable "vpc_name" {
     description = "name of the VPC" 
+    type = string
     
 }
 
