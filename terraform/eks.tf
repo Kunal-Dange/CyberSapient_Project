aa32409d58c8d46b8c48f7af3b14d425c5d902a5
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-04 11:09:22 +0530

    terraform

---
+++ terraform/eks.tf
@@ -0,0 +1,43 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.public_subnets
+  }
+
+  kubernetes_network_config {
+    service_ipv4_cidr = "172.20.0.0/16"
+  }
+
+  depends_on = [var.cluster_role_arn]
+}
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.private_subnets
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [aws_eks_cluster.this]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "oidc_provider_arn" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,10 +3,32 @@ variable "target_infra" {
     default     = "Nonprod"
 }
 
-# variable "environment" {
-#   description = "Environment name (prod/nonprod)"
-#   type        = string
-# }
-# variable "app_env" {
-#     description = "Application environment" 
-# }
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet IDs"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet IDs"
+}
+
+variable "cluster_role_arn" {
+  type        = string
+  description = "IAM Role ARN for the EKS cluster"
+}
+
+variable "node_role_arn" {
+  type        = string
+  description = "IAM Role ARN for worker nodes"
+}
