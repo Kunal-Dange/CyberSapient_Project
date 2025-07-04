559319907df3a8ae1c1c9e3db0926317e7fab2fa
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-04 11:38:04 +0530

    terraform

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.eks_cluster_role
 
   vpc_config {
-    subnet_ids = var.public_subnets
+    subnet_ids = [aws_subnet.public.*.id, aws_subnet.private.*.id]
   }
 
   kubernetes_network_config {
@@ -17,7 +17,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.private_subnets
+  subnet_ids      = [aws_subnet.public.*.id, aws_subnet.private.*.id]
 
   scaling_config {
     desired_size = 2
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,22 +8,8 @@ variable "cluster_name" {
   default = "cybersapient-eks"
 }
 
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID"
-}
-
 variable "vpc_name" {
   default = "cybersapient"
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "List of public subnet IDs"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of private subnet IDs"
-}
 
