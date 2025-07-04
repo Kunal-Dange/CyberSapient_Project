f90a02156ff90315184f7d9044e8949940eb93b7
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-04 11:23:37 +0530

    terraform

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = var.cluster_role_arn
+  name     = "${var.environment}-cluster-role"
+  role_arn = aws_iam_role.eks_cluster_role
 
   vpc_config {
     subnet_ids = var.public_subnets
@@ -16,7 +16,7 @@ resource "aws_eks_cluster" "this" {
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = var.node_role_arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.private_subnets
 
   scaling_config {
