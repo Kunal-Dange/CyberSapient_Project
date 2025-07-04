2d49b449b6d7a68ba6df3434b8becd81c025e9d7
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-04 12:48:00 +0530

    terraform

---
+++ terraform/namespace.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_namespace" "cybersapient_namespace" {
+  metadata {
+    annotations = {
+      name = "${var.environment}"
+    }
+
+    labels = {
+      mylabel = "label-value"
+    }
+
+    name = "${var.environment}"
+  }
+}
