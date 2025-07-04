86bfa2af6cd73fa5c2e5e208e4b3293687d62d5b
Kunal Dange <kunaldange@Kunals-MacBook-Air.local>
2025-07-04 11:01:15 +0530

    terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,54 +1,47 @@
-module "iam_roles" {
-  source = "./modules/iam_roles"
-  iam_roles = {
-    role1 = {
-      name                = "example-role"
-      assume_role_policy  = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-      description         = "Example IAM Role"
-      max_session_duration = 3600
-      tags = {
-        Environment = "Dev"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
       }
-    }
-  }
+      Action = "sts:AssumeRole"
+    }]
+  })
 
-  iam_role_policies = {
-    policy1 = {
-      name      = "example-policy"
-      role_name = "role1" # Use the key from iam_roles, not the role name
-      policy    = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "s3:*",
-      "Resource": "*"
-    }
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   ]
 }
-EOF
-    }
-  }
 
-  managed_policies = {
-    attachment1 = {
-      name      = "example-attachment"
-      role_name = "role1" # Use the key from iam_roles, not the role name
-      policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-    }
-  }
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
+}
+
+output "cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
+}
+
+output "node_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
 }
